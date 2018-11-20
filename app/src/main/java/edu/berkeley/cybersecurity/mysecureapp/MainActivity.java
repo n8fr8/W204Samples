0bc19761de949614817019f491ffd7edac2fef1e
n8fr8 <nathan@guardianproject.info>
2018-11-20 11:47:58 -0500

    add code

---
+++ app/src/main/java/edu/berkeley/cybersecurity/mysecureapp/MainActivity.java
@@ -0,0 +1,33 @@
+package edu.berkeley.cybersecurity.mysecureapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void doLogin ()
+    {
+
+    }
+
+    public void downloadData ()
+    {
+
+    }
+
+    public void submitData ()
+    {
+
+    }
+
+    public void doLogout ()
+    {
+
+    }
+}
---
+++ app/src/main/java/edu/berkeley/cybersecurity/mysecureapp/SecureNetworkAndStorageMainActivity.java
@@ -0,0 +1,33 @@
+package edu.berkeley.cybersecurity.mysecureapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SecureNetworkAndStorageMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void doLogin ()
+    {
+
+    }
+
+    public void downloadData ()
+    {
+
+    }
+
+    public void submitData ()
+    {
+
+    }
+
+    public void doLogout ()
+    {
+
+    }
+}
